415a131fedf5d2fad78c60ed6f4d454b9516fedd
Madalina <madalina@ubuntu.(none)>
2014-11-29 23:33:29 -0500

    Adding the edit functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,24 @@
 class UsersController < ApplicationController
+  #the before filter applies only to edit and update action 
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
+ def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+ end
+
+ def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user) 
+ end
+
   def new
   	@user = User.new
   end
+ 
   def create
   	@user = User.new(user_params)
   	if @user.save
@@ -12,8 +29,23 @@ class UsersController < ApplicationController
   		render 'new'
   	end
   end
+
   def show
   		@user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update 
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:succes] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   private
