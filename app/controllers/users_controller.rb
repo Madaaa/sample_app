de4be861dce1f0c90c619c6a568e1cab4a7c35e8
Madalina <madalina@ubuntu.(none)>
2014-11-29 21:02:37 -0500

    Adding log in functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
+      log_in @user
       flash[:succes] = "Welcome to the simple app!"
       redirect_to @user
   	else
