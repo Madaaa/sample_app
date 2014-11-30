6dc511a7393c98ffc51f374bb0c089ea40856745
Madalina <madalina@ubuntu.(none)>
2014-11-30 16:11:00 -0500

    Adding the listing users functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
   #the before filter applies only to edit and update action 
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
+  def index
+    @users = User.paginate(page: params[:page])
+  end
  def logged_in_user
     unless logged_in?
+      store_location
       flash[:danger] = "Please log in."
       redirect_to login_url
     end
@@ -48,9 +53,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:succes] = "User deleted"
+    redirect_to users_url
+  end
+
   private
 
 	  def user_params
 	  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
 	  end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
